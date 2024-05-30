65fc3dcbbf4db65b2db957caf74494bebeb1c59b
John <john@beyondvoip.net>
2024-05-30 10:11:43 -0700

    TEst

--- integrity_check_tool.py
+++ integrity_check_tool.py
@@ -104,7 +104,7 @@ def exit_handler(*args):
 def build_observer():
     patterns = ["*"]
     ignore_patterns = ["*git*"]
-    ignore_directories = False
+    ignore_directories = [".git"]
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
     my_event_handler.on_created = on_created
