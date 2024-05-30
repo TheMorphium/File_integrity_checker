978113463090e4ed7e4b57188b651bfb06069b01
John <john@beyondvoip.net>
2024-05-30 07:39:17 -0700

    Add preamble to ensure venv pip packages are imported

--- integrity_check_tool.py
+++ integrity_check_tool.py
@@ -2,8 +2,15 @@
 
 import os
 import site
+import sys
+
+python_version = sys.version_info
+python_lib_folder = f'/env/lib/python{sys.version_info[0]}.{sys.version_info[1]}/site-packages/'
+
+site.addsitedir(os.curdir + python_lib_folder)
+
+
+import sms_messaging
 
 from dotenv import load_dotenv
 
-import site
-site.addsitedir('/path/to/virtualenv/lib/python2.7/site-packages/')
--- sms_messaging.py
+++ sms_messaging.py
@@ -1,4 +1,11 @@
 import os
+import site
+import sys
+
+python_version = sys.version_info
+python_lib_folder = f'/env/lib/python{sys.version_info[0]}.{sys.version_info[1]}/site-packages/'
+
+site.addsitedir(os.curdir + python_lib_folder)
 
 from dotenv import load_dotenv
 from twilio.rest import Client
