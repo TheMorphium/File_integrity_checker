4c9ab18feba5269a36c0248cc51f7c8ca795ccbc
John <john@beyondvoip.net>
2024-05-30 07:54:12 -0700

    Add preamble to ensure venv pip packages are imported

--- integrity_check_tool.py
+++ integrity_check_tool.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import ast
 import os
 import site
 import sys
@@ -9,8 +10,52 @@ python_lib_folder = f'/env/lib/python{sys.version_info[0]}.{sys.version_info[1]}
 
 site.addsitedir(os.curdir + python_lib_folder)
 
-
-import sms_messaging
+from time import sleep
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 from dotenv import load_dotenv
 
+load_dotenv()
+
+locations = ast.literal_eval(os.getenv('locations'))
+
+def on_created(event):
+    print(f"hey, {event.src_path} has been created!")
+
+def on_deleted(event):
+    print(f"what the f**k! Someone deleted {event.src_path}!")
+
+def on_modified(event):
+    print(f"hey buddy, {event.src_path} has been modified")
+
+def on_moved(event):
+    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+
+def build_observer():
+    my_event_handler.on_created = on_created
+    my_event_handler.on_deleted = on_deleted
+    my_event_handler.on_modified = on_modified
+    my_event_handler.on_moved = on_moved
+    my_observer = Observer()
+    for location in locations:
+        my_observer.schedule(my_event_handler, location, recursive=True)
+    my_observer.start()
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
+
+
+
+
+
+if __name__ == "__main__":
+    patterns = ["*"]
+    ignore_patterns = None
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    build_observer()
